8f6a538699023cef9c2bbc37e659b399e3774247
Akshit Kumar <akshit2707.iitkgp@gmail.com>
2021-05-15 22:17:04 +0530

    Signup POST entity

---
+++ pjp3/src/main/java/com/sapient/pjp3/entity/Signup.java
@@ -0,0 +1,48 @@
+package com.sapient.pjp3.entity;
+
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import lombok.Data;
+
+@Data
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Signup{
+
+	private int id;
+	private String full_name;
+	private String email;
+	private String password;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFull_name() {
+		return full_name;
+	}
+	public void setFull_name(String full_name) {
+		this.full_name = full_name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
+
+
+
